8ced89e7e97dd11f57bcb2cf1f9b8dde03861c0b
unknown <lnazareno.dn@gmail.com>
2021-07-02 01:16:01 -0300

    tp5

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :color="pickedColor" :colorSelection="colorSelection"/>
+    <Navigator :message="messageDisplay" @restart="restart($event)" :restartBtn="restartBtnText" @colorCount="colorCount=$event" @isHardSelected="isHardSelected=$event" />
+    <div id="container">
+      <div v-for="(cuadrado, index) in colors" :key="index">
+        <Container :color="colors[index]" @squareSelected="eventClick($event)"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Navigator from './components/Navigator.vue'
+import Container from './components/Container.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Navigator,
+    Container
+  },
+  props:['isHardSelected', 'colorCount'],
+
+  data(){
+    return{
+      colors: [],
+      pickedColor: '',
+      messageDisplay: '',
+      colorSelection: '',
+      restartBtnText: '',
+
+
+    }
+  },
+  mounted(){
+    this.restart();
+    this.pickedColor = '';
+  },
+  methods: {
+    eventClick(squareColor){
+       squareColor = this.style.backgroundColor
+      if (squareColor === this.pickedColor) {
+        this.restartBtnText = "Play Again!"
+        this.messageDisplay = "You Picked Right!"
+        this.setColors(this.pickedColor);
+        this.colorSelection = this.pickedColor
+      } else {
+        this.messageDisplay = "Try Again!"
+        this.style.backgroundColor = "#232323"
+        this.messageDisplay.style.color = "#000000";
+      }
+    },
+    setColors(color){
+      for (let i = 0; i < this.colors.length; i++) {
+        this.colors[i] = color;
+      }
+    },
+
+    resetPick(){
+      var number;
+      if (this.isHardSelected) {
+        number = 6
+      } else{
+        number = 3
+      }
+      return Math.floor(Math.random() * number)
+    },
+
+    randomInt(){
+      return Math.floor(Math.random() * 256)
+    },  
+
+    createRgb(){
+      var newColor = "rgb(" + this.randomInt() + ", " + this.randomInt() + ", " + this.randomInt() +  ")"
+      return newColor
+    },
+
+    createNewColors(numbers){
+      var arr = [];
+        for (var i = 0; i < numbers; i++) {
+          arr.push(this.createRgb());
+        }
+        return arr;
+    },
+
+    restart(){
+      this.colors = this.createNewColors(this.colorCount)
+      this.messageDisplay = ""
+      this.restartBtnText = "NEW COLORS!"
+      this.pickedColor = this.colors[this.resetPick()]
+      this.colorSelection = "steelblue"
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #232323;
+	margin: 0;
+	font-family: "Montserrat", "Avenir";
 }
+body {
+	background: #232323;
+	margin: 0;
+	font-family: "Montserrat", "Avenir";
+}
+#container {
+	margin: 20px auto;
+	max-width: 600px;
+}
+
 </style>
---
+++ src/components/Navigator.vue
@@ -0,0 +1,105 @@
+<template>
+
+  <section class="src-components-navigator">
+    <div id="navigator">
+		  <button id="reset" @click="eventBtnRestart()">{{restartBtn}}</button>
+		  <span id="message"> {{message}}</span>
+
+	  	<button id="easy" :class="{selected: isEasySelected}" @click="eventBtnEasy()">easy</button>
+		  <button id="hard" :class="{selected: isHardSelected}" @click="eventBtnHard()">hard</button>
+	  </div>
+
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'src-components-navigator',
+    props: ['restartBtn', 'message'],
+    mounted () {
+
+    },
+    data () {
+      return {
+        colorCount: 6,
+        isEasySelected: false,
+        isHardSelected: true 
+      }
+    },
+    methods: {
+      eventBtnEasy(){
+        if (isHardSelected) {
+          this.isHardSelected = false
+          this.isEasySelected = true
+          this.colorCount = 3
+          this.$emit('colorCount', this.colorCount)
+          this.$emit('isHardSelected', this.isHardSelected)
+          this.$emit('restart', this.colorCount)
+        }
+      },
+      eventBtnHard(){
+        if (!isHardSelected) {
+          this.isHardSelected = true
+          this.isEasySelected = false
+          this.colorCount = 6
+          this.$emit('colorCount', this.colorCount)
+          this.$emit('isHardSelected', this.isHardSelected)
+          this.$emit('restart', this.colorCount)
+        }
+      },
+      eventBtnRestart(){
+        this.$emit('restart', this.colorCount)
+      }
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+  .src-components-navigator {
+
+  }
+
+  .selected {
+	  background-color: steelblue;
+	  color: white;
+  }
+
+  button {
+	  border: none;
+	  background-color: white;
+	  font-family: "Montserrat", "Avenir";
+	  text-transform: uppercase;
+	  height: 100%;
+	  font-weight: 700;
+	  letter-spacing: 1px;
+	  color: steelblue;
+	  transition: all 0.3s;
+	  outline: none;
+  }
+
+  button:hover {
+	  color: white;
+	  background-color: steelblue;  
+  }
+
+  #message {
+	  color: #ffffff;
+	  display: inline-block;
+	  width: 20%;
+  }
+
+  #navigator {
+	  background: #ffffff;
+	  height: 30px;
+	  text-align: center;
+	  margin: 0;
+	  margin-top: -30px;
+  }
+</style>
